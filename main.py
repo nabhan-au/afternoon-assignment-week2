ae696d3a66f01f8cd6bbabf029fa295617148ae4
Nabhan Suwanachote <nabhan.au@gmail.com>
2022-02-06 15:38:15 +0700

    complete add reservation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, HTTPException
+from bson import json_util
 from pymongo import MongoClient
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 class Reservation(BaseModel):
     name : str
@@ -10,10 +14,10 @@ class Reservation(BaseModel):
 client = MongoClient('mongodb://localhost', 27017)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["test"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["test"]
 
 app = FastAPI()
 
@@ -29,7 +33,13 @@ def get_reservation_by_table(table: int):
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
-    pass
+    result = collection.find({"time": {"$eq": reservation.time}, "table_number": reservation.table_number})
+    size = len(list(result))
+    if size == 0:
+        reserve = jsonable_encoder(reservation)
+        collection.insert_one(reserve)
+    else:
+        raise HTTPException(404, f"{reservation.time} is already reserved")
 
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
