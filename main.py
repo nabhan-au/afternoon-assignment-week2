76288bdfa7a077c28fae45418f511792bb211484
Nabhan Suwanachote <nabhan.au@gmail.com>
2022-02-06 16:18:59 +0700

    finish update and delete

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_reservation_by_table(table: int):
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
-    result = collection.find({"time": {"$eq": reservation.time}, "table_number": reservation.table_number})
+    result = collection.find({"time":reservation.time, "table_number": reservation.table_number})
     size = len(list(result))
     if size == 0:
         reserve = jsonable_encoder(reservation)
@@ -57,11 +57,19 @@ def reserve(reservation : Reservation):
     else:
         raise HTTPException(404, f"{reservation.time} is already reserved")
 
-@app.put("/reservation/update/")
-def update_reservation(reservation: Reservation):
-    pass
+@app.put("/reservation/update/{new_time}")
+def update_reservation(reservation: Reservation, new_time: int):
+    result = collection.find({"time":int(new_time), "table_number": reservation.table_number})
+    lst_result = list(result)
+    if len(lst_result) == 0:
+        my_query = {"time":reservation.time, "table_number": reservation.table_number}
+        new_query = {"$set":{"time":new_time}}
+        collection.update_one(my_query, new_query)
+    else:
+        return HTTPException(404, f"Reservation time is already reserved at: {reservation.time}'")
+    
 
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
-    pass
+    collection.delete_one({"name":name, "table_number":table_number})
 
